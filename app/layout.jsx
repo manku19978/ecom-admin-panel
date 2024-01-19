3993dcfabf54fea27402ccbf5f3e858f5bc51d9c
manku19978 <mayanksingh1997@gmail.com>
2024-01-19 17:06:09 +0530

    basic styles setup along + lint fix + custom route added

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import { Roboto_Mono } from "next/font/google";
+import Sidebar from "@/src/components/Sidebar/Sidebar";
+import cn from "classnames";
+import "@/src/assets/styles/variables.css";
+import "@/src/assets/styles/global.css";
+import "@/src/assets/styles/utilities.css";
+
+const roboto = Roboto_Mono({
+  subsets: ["latin"],
+  weights: [400, 500, 600, 700],
+});
+
+export const metadata = {
+  title: "Ecom admin panel",
+  description: "Handle tasks related to your ecom website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={cn(roboto.className, "mainContainer")}>
+        <Sidebar />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,8 @@
+import cn from "classnames";
+import s from "./Sidebar.module.css";
+
+const Sidebar = () => {
+  return <div className={cn(s.container)}></div>;
+};
+
+export default Sidebar;
